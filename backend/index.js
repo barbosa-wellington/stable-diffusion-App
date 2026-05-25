1a7f211ca1a825b0b6f12519b6fa0932d5a5c4be
Barbosa <wellb.shadow@hotmail.com>
2026-05-25 18:08:52 +1000

    Update index

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Allows server to understand json sent from the frontend
+// Allows your server to understand JSON data sent from the mobile app
 app.use(express.json());
 
 // A test route to make sure it works
 app.get('/api/test', (req, res) => {
-    res.json({message: "Hello for Stable App"});
+  res.json({ message: "Hello from the backend server!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
