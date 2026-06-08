e4a3c747b90366b335b624341dd6661b1584139e
Barbosa <wellb.shadow@hotmail.com>
2026-06-08 16:43:06 +1000

    Update 3d processing
    Applying techniques of geometry and camera to generate a 3D view of a given asset.

--- Processing/01_basics_o3d.py
+++ Processing/01_basics_o3d.py
@@ -10,21 +10,35 @@ import open3d as o3d
 import copy
 
 #load data in variables
-# pcd = o3d.io.read_point_cloud('bunny/bun_zipper_res2.ply')
+pcd = o3d.io.read_point_cloud('data/bun_zipper_res2.ply')
 # pcd_original = o3d.io.read_point_cloud('dragon_stand/dragonStandRight_336.ply')
 forest = o3d.io.read_image('data/forest-scene.png')
 forest_depth = o3d.io.read_image('data/image.png')
 
-print('This is the original image')
-print(forest)
+# print('This is the original image')
+# print(forest)
 
-print('This is the image depth')
-print(forest_depth)
+# print('This is the image depth')
+# print(forest_depth)
 
-# o3d.visualization.draw_geometries([forest])
 
+# method to visualize the image on a 3D view
+def generate_3D_view(img1,img2,):
+
+    # creating a RGBD image that combines both original and depth image for a 3d visualization
+    forest_RBGD = o3d.geometry.RGBDImage.create_from_color_and_depth(img1, img2,convert_rgb_to_intensity=False)
+    print(forest_RBGD)
+
+    forest_camera = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.Kinect2DepthCameraDefault)
+
+    # creating from Pointcloud
+    forest_pointcloud = o3d.geometry.PointCloud.create_from_rgbd_image(forest_RBGD,forest_camera)
+
+    o3d.visualization.draw_geometries([forest_pointcloud])
+
+generate_3D_view(forest, forest_depth)
 # #visualization of the data
-# #o3d.visualization.draw_geometries([pcd1])
+# o3d.visualization.draw_geometries([pcd])
 
 # #combining both models
 # #o3d.visualization.draw_geometries([pcd, pcd1])
